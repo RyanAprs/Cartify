8e684db6e05768a82da30e946f52b2e967adf9db
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-11-25 21:43:50 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen max-h-fit bg-blue-400 flex justify-center items-center">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
